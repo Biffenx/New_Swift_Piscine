3914bf715ab2b59776bc1e1f4cfe9c405eb7d1ae
jwilen <jochum.wilen@gmail.com>
2021-06-12 17:25:11 +0300

    ex01 done2

--- D00/D00/ViewController.swift
+++ D00/D00/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController {
     let message = "You have clicked Me!"
     
     @IBOutlet weak var label1: UILabel!
-    
+        
     @IBAction func Btn1(_ sender: UIButton) {
         label1.font = UIFont(name:"Didot", size: 30.0)
+        Alert()
     }
     @IBAction func Btn2(_ sender: UIButton) {
         label1.font = UIFont(name:"Times new Roman", size: 30.0)
+        Alert()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+  func Alert() {
+    let alert = UIAlertController(title: "Notice", message: "The font was changed.", preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//    alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+    self.present(alert, animated: true)
+    }
 }
 
