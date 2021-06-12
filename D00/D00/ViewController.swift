3ba96e77a62c8fb5a9bb20b04b295156e4f4359d
jwilen <jochum.wilen@gmail.com>
2021-06-12 17:15:18 +0300

    ex01 done

--- D00/D00/ViewController.swift
+++ D00/D00/ViewController.swift
@@ -13,12 +13,17 @@ class ViewController: UIViewController {
     let message = "You have clicked Me!"
     
     @IBOutlet weak var label1: UILabel!
+    
     @IBAction func Btn1(_ sender: UIButton) {
-        print(message)
-        label1.text = message
+        label1.font = UIFont(name:"Didot", size: 30.0)
+    }
+    @IBAction func Btn2(_ sender: UIButton) {
+        label1.font = UIFont(name:"Times new Roman", size: 30.0)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+    label1.text = message
+
         // Do any additional setup after loading the view.
     }
 
