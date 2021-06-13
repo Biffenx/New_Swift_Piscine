6c0411fb4c20166f35fd489936a0e598ea9126ec
jwilen <jochum.wilen@gmail.com>
2021-06-13 13:55:52 +0300

    water app2

--- D00_Stay_Hydrated/D00_Stay_Hydrated/ViewController.swift
+++ D00_Stay_Hydrated/D00_Stay_Hydrated/ViewController.swift
@@ -14,18 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var progessBar: UIProgressView!
     
     let progress = Progress(totalUnitCount: 6)
-    
-  
     var waterConsumed = 0
     var timer: Timer?
    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("1:\(waterConsumed)")
         progessBar.transform = progessBar.transform.scaledBy(x: 1, y: 30)
-
         
     }
 
@@ -33,9 +29,6 @@ class ViewController: UIViewController {
     progessBar.progress = 0
     timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(drinkWaterWarning), userInfo: nil, repeats: true)
     print("3:\(waterConsumed)")
-//    if waterConsumed > 20 {
-//            timer?.invalidate()
-//                 }
     //    guard self.progress.isFinished == false else {
 //        timer?.invalidate()
 //        print("Finnish")
@@ -65,9 +58,6 @@ class ViewController: UIViewController {
              }
         }))
         alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-//        if waterConsumed > 20 {
-//                  timer?.invalidate()
-//              }
         self.present(alert, animated: true)
     }
     
